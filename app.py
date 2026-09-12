d46a22b2397037a2e53a34f23997dcafb67fa659
Stephen Tierney <sjtrny@gmail.com>
2026-09-12 13:40:02 +0000

    Simplify speed test implementation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import asyncio
 import os
+from contextlib import suppress
 from pathlib import Path
 
 from aiohttp import web
@@ -26,20 +26,17 @@ async def download(request):
             "X-Accel-Buffering": "no",
         }
     )
-    try:
+    with suppress(OSError):
         await response.prepare(request)
         while True:
             await response.write(DATA)
-    except OSError:
-        raise asyncio.CancelledError
+    return response
 
 
 async def upload(request):
-    try:
+    with suppress(OSError):
         async for _ in request.content.iter_any():
             pass
-    except OSError:
-        raise asyncio.CancelledError
     return web.Response(status=204, headers=NO_STORE)
 
 
